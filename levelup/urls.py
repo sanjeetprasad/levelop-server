be3a54ea5433d64e2f9507ef34bfc0ae82c9ec96
Sanjeet Prasad <sanjeet.l.prasad@gmail.com>
2021-02-09 16:12:20 -0600

    listed the games on the dom, by updating the games in games.json, games.py,added the serializer in the game.py and updated the urls gametype and Gameview

--- levelup/urls.py
+++ levelup/urls.py
@@ -2,11 +2,13 @@ from django.conf.urls import include
 from django.urls import path
 from levelupapi.views import register_user, login_user
 from rest_framework import routers
-from levelupapi.views import GameTypes
+from levelupapi.views import GameTypes, Gameview
+
 
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'gametypes', GameTypes, 'gametype')
+router.register(r'games', Gameview, 'game')
 
 
 urlpatterns = [
--- levelupapi/models/games.py
+++ levelupapi/models/games.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class Games(models.Model):
-
+    title = models.CharField(max_length=75)
+    maker = models.CharField(max_length=20)
     gamer = models.ForeignKey("Gamer", on_delete=models.CASCADE)
     gametype = models.ForeignKey("GameType", on_delete=models.CASCADE)
-    title = models.CharField(max_length=75)
-    description = models.CharField(max_length=255)
     number_of_players = models.IntegerField()
+    skill_level = models.IntegerField()
